80029d3022c4bfa40eff43dc646e9dbe04ccbe26
deepak-deve <vrisha142@gmail.com>
2026-05-12 14:03:17 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE task_db;
+USE task_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    username VARCHAR(50),
+    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
+);
